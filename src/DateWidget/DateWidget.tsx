cf3fcec4dbeddc612b9a7d00c6733b749734ebdc
Andrew Che <andrewche@berkeley.edu>
2020-05-18 19:30:05 -0700

    Fixed formatting

--- src/DateWidget/DateWidget.tsx
+++ src/DateWidget/DateWidget.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { KeyboardDatePicker } from '@material-ui/pickers';
 import { WidgetProps } from 'react-jsonschema-form';
+import { FormControl } from '@material-ui/core';
+import moment from 'moment';
 
 const DateWidget = ({
   id,
   readonly,
+  required,
   disabled,
   label,
   value,
@@ -12,24 +15,31 @@ const DateWidget = ({
   schema,
 }: WidgetProps) => {
   const _onChange = (_date: any, value?: string | null | undefined): void => {
-    onChange(value);
+    onChange(moment(value).format('YYYY-MM-DD'));
   };
 
+  const _displayValue = moment(value).format('MM/DD/YYYY');
+
   return (
-    <KeyboardDatePicker
-      autoOk
-      variant="inline"
-      format="YYYY-MM-DD"
-      margin="normal"
-      id={id}
-      label={label || schema.title}
-      disabled={disabled || readonly}
-      value={value ? value : ''}
-      onChange={_onChange}
-      KeyboardButtonProps={{
-        'aria-label': 'change date',
-      }}
-    />
+    <FormControl fullWidth={true} required={required}>
+      <KeyboardDatePicker
+        autoOk
+        variant="inline"
+        format="MM/DD/YYYY"
+        margin="normal"
+        id={id}
+        label={label || schema.title}
+        disabled={disabled || readonly}
+        value={_displayValue}
+        onChange={_onChange}
+        KeyboardButtonProps={{
+          'aria-label': 'change date',
+        }}
+        disablePast
+        error={false}
+        helperText={null}
+      />
+    </FormControl>
   );
 };
 
