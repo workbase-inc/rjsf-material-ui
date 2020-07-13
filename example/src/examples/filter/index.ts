90a5c2d4283781eae9bc3dd429d2085cd0ab38e5
Ed Parsons <ed@acutulus.co>
2020-07-13 10:30:04 -0700

    Adding filter

---
+++ example/src/examples/filter/index.ts
@@ -0,0 +1,11 @@
+import schema from './schema.json';
+import uiSchema from './ui-schema.json';
+import formData from './form-data.json';
+
+export default {
+  key: 'filter',
+  title: 'Filter',
+  schema,
+  uiSchema,
+  formData,
+};
